dbbf4a3e71bf5547ddfde16bac74e81414d5a5a1
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-02-08 18:19:06 +0000

    Add routers to index

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import 'express-async-errors';
 import * as methodOverride from "method-override";
 import {static as eStatic, urlencoded} from "express";
 import {engine} from "express-handlebars";
+import {homeRouter} from "./routers/home";
+import {warriorRouter} from "./routers/warrior";
+import {arenaRouter} from "./routers/arena";
+import {hallOfFameRouter} from "./routers/hall-of-fame";
 
 
 const app = express();
@@ -17,9 +21,12 @@ app.engine('.hbs', engine({
     // helpers: ????
 }));
 app.set('view engine', '.hbs');
-app.get('/', (req,res) => {
-    res.send('Hello');
-});
+
+app.use('/', homeRouter);
+app.use('/warrior', warriorRouter);
+app.use('/arena', arenaRouter);
+app.use('hall-of-fame', hallOfFameRouter);
+
 // app.use(handleError)
 
 app.listen(3000, 'localhost', () => {
