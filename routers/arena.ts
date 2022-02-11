f62f01520695c2a575cb7d9046eb7bd52441821e
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-02-11 13:19:51 +0000

    Add arena and hall routers

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,13 +1,43 @@
 import { Router } from "express";
+import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/errors";
+import {fight} from "../utils/fight";
 
 export const arenaRouter = Router();
 
 arenaRouter
 
-    .get('/fight-form', (req,res) => {
-        res.render('arena/fight-form');
+    .get('/fight-form', async (req,res) => {
+        const warriors = await WarriorRecord.listAll();
+        res.render('arena/fight-form', {
+            warriors
+        });
     })
 
-    .post('/fight',(req,res) => {
-        res.render('arena/fight');
+    .post('/fight',async (req,res) => {
+
+        const {warrior1: warrior1Id, warrior2: warrior2Id} = req.body;
+
+        if (warrior1Id === warrior2Id){
+            throw new ValidationError(`Please choose two different opponents.`);
+        }
+
+        const warrior1 = await WarriorRecord.getOne(warrior1Id);
+        const warrior2 = await WarriorRecord.getOne(warrior2Id);
+
+        if (!warrior1) {
+            throw new ValidationError(`opponent number 1 was not found.`);
+        }
+        if (!warrior2) {
+            throw new ValidationError(`opponent number 2 was not found.`);
+        }
+
+        const {log, winner} = fight(warrior1, warrior2);
+
+        winner.wins++;
+        await winner.update();
+
+        res.render('arena/fight', {
+            log,
+        });
     })
--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
+import {WarriorRecord} from "../records/warrior.record";
 
 export const hallOfFameRouter = Router();
 
 hallOfFameRouter
 
 
-    .get('/', (req,res) => {
-        res.render('hall-of-fame/list');
+    .get('/', async (req,res) => {
+        const warriors = (await WarriorRecord.listTop(10)
+        ).map((warrior, i) => {
+            return {
+                place: i + 1,
+                warrior,
+            }
+        });
+        res.render('hall-of-fame/list', {
+            warriors
+        });
     })
